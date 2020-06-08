e7ef9ee5c69be22091b1a2fe34c27662117a8f18
ms2138 <m38371@gmail.com>
2020-06-07 19:36:29 -0700

    Initial commit

---
+++ Scale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scale
+//
+//  Created by mani on 2020-06-07.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
