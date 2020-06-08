b75448b14b02c5dac67fb04e5198ce1ee0dd5128
ms2138 <m38371@gmail.com>
2020-06-07 19:56:19 -0700

    Rename ViewController to WeightViewController.  Change UIViewController custom class to WeightViewController.  Add UINavigationController to Main.storyboard.

--- Scale/Controllers/WeightViewController.swift
+++ Scale/Controllers/WeightViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeightViewController.swift
 //  Scale
 //
 //  Created by mani on 2020-06-07.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeightViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
