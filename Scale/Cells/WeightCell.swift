ca2ecee96a4b4f04f8390c871eff3b18346544de
ms2138 <m38371@gmail.com>
2020-06-07 23:01:59 -0700

    Add Cells folder.  Add WeightCell class to Cells folder

---
+++ Scale/Cells/WeightCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeightCell.swift
+//  Scale
+//
+//  Created by mani on 2020-06-07.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class WeightCell: UITableViewCell {
+
+}
