6379c011ee449cdb2b30c9a6e6023ac6e9ccd934
ms2138 <m38371@gmail.com>
2020-06-12 23:31:33 -0700

    Add AddWeightViewController class

---
+++ Scale/Controllers/AddWeightViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddWeightViewController.swift
+//  Scale
+//
+//  Created by mani on 2020-06-12.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class AddWeightViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
