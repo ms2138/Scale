e6fa2cd5be6a84e15283ad7990430160412f0890
ms2138 <m38371@gmail.com>
2020-06-13 23:48:14 -0700

    Add weight, overallChange, and date properties to WeightDetailsViewController

--- Scale/Controllers/WeightDetailsViewController.swift
+++ Scale/Controllers/WeightDetailsViewController.swift
@@ -12,7 +12,9 @@ class WeightDetailsViewController: UITableViewController {
     @IBOutlet weak var dateDescriptionCell: UITableViewCell!
     @IBOutlet weak var weightDescriptionCell: UITableViewCell!
     @IBOutlet weak var overallChangeCell: UITableViewCell!
-    
+    var weight: String?
+    var overallChange: Float?
+    var date: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
