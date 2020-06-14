2c9c0ac0fd63e5b7b80c8c9268556c52a4aaf781
ms2138 <m38371@gmail.com>
2020-06-13 23:32:35 -0700

    Add WeightDetailsViewController class

---
+++ Scale/Controllers/WeightDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeightDetailsViewController.swift
+//  Scale
+//
+//  Created by mani on 2020-06-13.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class WeightDetailsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
