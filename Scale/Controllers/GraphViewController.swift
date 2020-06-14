43c33f4a00988f1338ccb95c86fb11cdfdd319cb
ms2138 <m38371@gmail.com>
2020-06-14 14:07:13 -0700

    Add GraphViewController class

---
+++ Scale/Controllers/GraphViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GraphViewController.swift
+//  Scale
+//
+//  Created by mani on 2020-06-14.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
