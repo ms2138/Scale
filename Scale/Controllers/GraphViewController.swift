49fcf50d658caf8eb04fc06f2d7f4b7af048f36e
ms2138 <m38371@gmail.com>
2020-06-14 19:01:18 -0700

    Add initGraph method and make call to initGraph in GraphViewController's viewDidLoad method

--- Scale/Controllers/GraphViewController.swift
+++ Scale/Controllers/GraphViewController.swift
@@ -17,5 +17,25 @@ class GraphViewController: UIViewController {
         super.viewDidLoad()
 
         title = "Graph"
+
+        initGraph()
+    }
+}
+
+private extension GraphViewController {
+    func initGraph() {
+        let graph = CPTXYGraph(frame: hostView.bounds)
+        graph.plotAreaFrame?.masksToBorder = false
+        graph.plotAreaFrame?.paddingLeft = 20.0
+        graph.plotAreaFrame?.paddingBottom = 20.0
+        graph.paddingBottom = 30.0
+        graph.paddingLeft = 40.0
+        graph.paddingTop = 30.0
+        graph.paddingRight = 40.0
+        graph.titlePlotAreaFrameAnchor = .top
+        graph.backgroundColor = UIColor.black.cgColor
+        graph.titleDisplacement = CGPoint(x: 0.0, y: 30.0)
+
+        hostView.hostedGraph = graph
     }
 }
