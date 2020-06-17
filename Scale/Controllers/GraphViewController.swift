362e4f1d11ab919a21f1da0b65d66cefc9ddeb85
ms2138 <m38371@gmail.com>
2020-06-16 19:50:17 -0700

    Add setupAxis method to setup x and y axis on the graph.  Add configurePlotSpace method to configure the plot range of the graph.  Add calls to setupAxis and configurePlotSpace GraphViewController's viewDidLoad method

--- Scale/Controllers/GraphViewController.swift
+++ Scale/Controllers/GraphViewController.swift
@@ -20,6 +20,9 @@ class GraphViewController: UIViewController {
 
         initGraph()
         initScatterPlot()
+        setupAxis()
+
+        configurePlotSpace()
     }
 }
 
@@ -60,6 +63,68 @@ private extension GraphViewController {
         guard let graph = hostView.hostedGraph else { return }
         plot.dataSource = (self as CPTPlotDataSource)
         graph.add(plot, to: graph.defaultPlotSpace)
+    }
+
+    func setupAxis() {
+        guard let graph = hostView.hostedGraph else { return }
+        let axisSet = graph.axisSet as! CPTXYAxisSet
+
+        let titleStyle = CPTMutableTextStyle()
+        titleStyle.color = CPTColor.white()
+        titleStyle.fontName = "HelveticaNeue-Bold"
+        titleStyle.fontSize = 10.0
+        titleStyle.textAlignment = .center
+
+        let textStyle = CPTMutableTextStyle()
+        textStyle.color = CPTColor.white()
+        textStyle.fontName = "HelveticaNeue-Bold"
+        textStyle.fontSize = 10.0
+        textStyle.textAlignment = .center
+
+        let lineStyle = CPTMutableLineStyle()
+        lineStyle.lineColor = CPTColor.white()
+        lineStyle.lineWidth = 5
+
+        let gridLineStyle = CPTMutableLineStyle()
+        gridLineStyle.lineColor = CPTColor.gray()
+        gridLineStyle.lineWidth = 0.5
+
+
+        if let x = axisSet.xAxis {
+            x.title = "Number of days"
+            x.titleTextStyle = titleStyle
+            x.titleOffset = 25.0
+            x.labelTextStyle = textStyle
+            x.axisLineStyle = lineStyle
+            x.axisConstraints = CPTConstraints(lowerOffset: 0.0)
+        }
+
+        if let y = axisSet.yAxis {
+            y.title = "Weight"
+            y.titleOffset = 38.0
+            y.titleTextStyle = titleStyle
+            y.majorIntervalLength   = 5
+            y.minorTicksPerInterval = 2
+            y.labelTextStyle = textStyle
+            y.alternatingBandFills = [CPTFill(color: CPTColor.init(componentRed: 240.0, green: 240.0, blue: 240.0, alpha: 0.05)),
+                                      CPTFill(color: CPTColor.black())]
+            y.axisLineStyle = lineStyle
+            y.axisConstraints = CPTConstraints(lowerOffset: 0.0)
+        }
+    }
+
+    func configurePlotSpace() {
+        guard let graph = hostView.hostedGraph else { return }
+        guard let graphData = graphData else { return }
+        
+        let xMin = 1.0
+        let xMax = Double(graphData.count)
+        let yMin = Double(graphData[graphData.count - 1].weight) / 2
+        let yMax = Double(graphData[graphData.count - 1].weight)
+
+        guard let plotSpace = graph.defaultPlotSpace as? CPTXYPlotSpace else { return }
+        plotSpace.xRange = CPTPlotRange(locationDecimal: CPTDecimalFromDouble(xMin), lengthDecimal: CPTDecimalFromDouble(xMax))
+        plotSpace.yRange = CPTPlotRange(locationDecimal: CPTDecimalFromDouble(yMin), lengthDecimal: CPTDecimalFromDouble(yMax))
     }
 }
 
