702628d8f67ac7dbf039cf5eff8c4e66ba194fe9
ms2138 <m38371@gmail.com>
2020-06-17 20:20:06 -0700

    Add SettingsViewController class

---
+++ Scale/Controllers/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Scale
+//
+//  Created by mani on 2020-06-17.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
