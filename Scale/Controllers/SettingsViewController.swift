38a6e69c22b86e26f12ca7a6b00b16ed4229aced
ms2138 <m38371@gmail.com>
2020-06-21 00:01:45 -0700

    Add resetCellsAccessoryType method.  Add functionality to allow user to select between pounds and kilograms in the table view.  Set title to Settings in SettingsViewController

--- Scale/Controllers/SettingsViewController.swift
+++ Scale/Controllers/SettingsViewController.swift
@@ -11,5 +11,26 @@ import UIKit
 class SettingsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        title = "Settings"
+    }
+}
+
+extension SettingsViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        resetCellsAccessoryType()
+
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+private extension SettingsViewController {
+    func resetCellsAccessoryType() {
+        for i in 0..<tableView.numberOfRows(inSection: 0) {
+            if let cell = tableView.cellForRow(at: IndexPath(row: i, section: 0)) {
+                cell.accessoryType = .none
+            }
+        }
     }
 }
