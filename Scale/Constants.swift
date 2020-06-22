8ebac781f4f80eadba64dcdab8c6a499ce09456c
ms2138 <m38371@gmail.com>
2020-06-21 18:58:43 -0700

    Add Constants file.  Add kFirstTimeLaunchKey to Constants

---
+++ Scale/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Scale
+//
+//  Created by mani on 2020-06-21.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+let kFirstTimeLaunchKey = "FirstTimeLaunch"
