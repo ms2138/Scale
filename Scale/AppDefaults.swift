001b6972cebc8f297a9bd9872fe94512077a0de5
ms2138 <m38371@gmail.com>
2020-06-27 20:20:48 -0700

    Add defaults, firstTimeLaunched, and unitOfWeight properties too AppDefaults

--- Scale/AppDefaults.swift
+++ Scale/AppDefaults.swift
@@ -9,5 +9,23 @@
 import Foundation
 
 class AppDefaults {
-
+    private var defaults: UserDefaults {
+        return UserDefaults.standard
+    }
+    var firstTimeLaunched: Bool {
+        get {
+            return defaults.bool(forKey: Constants.firstTimeLaunched)
+        }
+        set {
+            defaults.set(newValue, forKey: Constants.firstTimeLaunched)
+        }
+    }
+    var unitOfWeight: UnitOfWeight {
+        get {
+            return UnitOfWeight(rawValue: defaults.integer(forKey: Constants.unitOfWeight)) ?? UnitOfWeight.pounds
+        }
+        set {
+            defaults.set(newValue.rawValue, forKey: Constants.unitOfWeight)
+        }
+    }
 }
